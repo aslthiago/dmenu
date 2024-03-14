c3e77770c865c4e2bb039026e30be0402fec1ac2
Thiago Lopes <aslthiago@gmail.com>
2024-03-13 21:00:39 -0300

    Update color scheme to use black, gray, and gold

--- config.def.h
+++ config.def.h
@@ -6,12 +6,15 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static const char *fonts[] = {
 	"Fira Code:size=10"
 };
+static const char col_black[]  = "#000000";
+static const char col_gray[]   = "#5e5e5e";
+static const char col_gold[]   = "#e59c19";
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*               fg         bg       */
+	[SchemeNorm] = { col_gray,  col_black },
+	[SchemeSel]  = { col_gold,  col_black },
+	[SchemeOut]  = { col_black, col_black },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
